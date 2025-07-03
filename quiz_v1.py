57de0787510ca07fe17a8800ac050181753aff41
Rajwinder Singh <irehmatsidhu@gmail.com>
2025-07-03 23:34:41 +0530

    Game v1 project
    This commit adds the Quiz Game v1 project, which includes three Python coding questions with multiple-choice options. The game allows users to start the quiz, answer questions, view correct answers, and see their final score. It uses lists, dictionaries, loops, conditionals, and user input for full interaction.

---
+++ quiz_v1.py
@@ -0,0 +1,45 @@
+print("==== Welcome To Quiz Game ====")
+questions = [
+    {
+        "question" : "which of the following is used to define a function in Python?",
+        "options" : ["1. Function", "2. Def", "3. Define", "4. Func"],
+        "answer" : "2" 
+    },
+    {
+        "question" : "What will be the output of this code?\n x = [1, 2, 3]\n print(len(x))",
+        "options" : ["1. 2", "2. 3", "3. 4", "4. Error"],
+        "answer" : "3"
+    },
+    {
+        "question" : "What will be the output of this code?\n x = \"python\"\n print(x[0])",
+        "options" : ["1. P", "2. y", "3. n", "4. x"],
+        "answer" : "1"
+    }
+]
+while True:
+    print("1. Start The Quiz")
+    print("2. Exit")
+    choose_option = int(input("Choose An Option: "))
+    if choose_option == 2:
+        print("Exiting...")
+        print("Goodbye")
+        break
+    elif choose_option == 1:
+        score = 0
+        for q in questions:
+            print(q["question"])
+            for option in q["options"]:
+                print(option)
+            ans_choose = input("Enter Your Option: ")
+            if ans_choose == q["answer"]:
+                print("Correct Answer!")
+                score +=1
+            elif ans_choose != q["answer"]:  
+                correct_answer = q["answer"]                
+                print(f"Wrong Answer! Correct Option is {correct_answer}")
+            else :
+                print("Error Choose The Correct Option")
+        print("Game Over!")
+        print(f"Your Total Score: {score} / {len(questions)}")    
+    else:
+        print("Error Try Again")
